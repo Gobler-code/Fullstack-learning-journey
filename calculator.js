d728d38e821b0c1c3f8754271103911ba4efc2ce
Gobler-code <goblergoss@gmail.com>
2026-05-19 15:23:46 +0545

    created a http server that add number using url parsing

--- calculator.js
+++ calculator.js
@@ -1,14 +1,26 @@
+const http = require('http');
+const PORT = 2000;
 
-const add =(a,b)=>{
-return a+b;
-}
+const server = http.createServer((req,res)=>{
 
-const subtract=(c,d)=>{
-    return c-d;
-}
+     const baseURL = `http://${req.headers.host}`;
+     const parsedURL = new URL(req.url , baseURL);
 
+     const pathname  = parsedURL.pathname;
+     const searchParams = parsedURL.searchParams;
 
-module.exports ={
-    add: add,
-    subtract: subtract,
-}
+     if(pathname === '/add' && req.method ==='GET')
+     {
+        const number1 = searchParams.get('num1') || 'nothing';
+        const number2 = searchParams.get('num2')  || 'nothing';
+
+        res.writeHead(200, {'Content_Type': 'application/Json'});
+        const total = Number(number1 )+ Number(number2);
+        return res.end(`the result is ${total}`);
+     }
+
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+})
+
+server.listen(PORT, () => console.log(`🚀 Running on http://localhost:${PORT}`));
