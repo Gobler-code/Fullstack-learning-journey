ed2fcc0a9111f7e79ba760e562c792f83a26a603
Gobler-code <goblergoss@gmail.com>
2026-05-20 15:39:02 +0545

    updated

--- main.js
+++ main.js
@@ -8,12 +8,5 @@ const greeting = require('./messages');
 
 // Task 3: Use the imported modules
 console.log(greeting); 
-console.log('Addition result:', calc.add(10, 5)); // <-- Correct function call
-console.log('Subtraction result:', calc.subtract(100, 25)); // <-- Correct function call
 
 
-// --- Task 4: Mini Real-World Task - Module Caching ---
-// The module loader checks if ./calculator has been loaded. It has.
-// Therefore, it returns the EXACT SAME object reference.
-const anotherCalc = require('./calculator');
-console.log('Task 4 Caching Test: Are the two calculator objects the same?', anotherCalc === calc);
