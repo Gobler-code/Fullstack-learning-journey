548703409781ce4e49b01cff0c59fe03d4053300
Gobler-code <goblergoss@gmail.com>
2026-05-20 15:37:30 +0545

    Updated the file

--- calculator.js
+++ calculator.js
@@ -14,12 +14,12 @@ const server = http.createServer((req,res)=>{
         const number1 = searchParams.get('num1') || 'nothing';
         const number2 = searchParams.get('num2')  || 'nothing';
 
-        res.writeHead(200, {'Content_Type': 'application/Json'});
+        res.writeHead(200, {'Content_Type': 'text/plain'});
         const total = Number(number1 )+ Number(number2);
         return res.end(`the result is ${total}`);
      }
 
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.writeHead(404, { 'Content_Type': 'text/plain' });
     res.end('404 Not Found');
 })
 
