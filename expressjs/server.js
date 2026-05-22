e6c0a4d8b9791a4420b95e7c49daa3081c2fb890
Gobler-code <goblergoss@gmail.com>
2026-05-22 22:14:21 +0545

    Learning express day 1

--- expressjs/server.js
+++ expressjs/server.js
@@ -1,1 +1,11 @@
 // Express server will go here
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.json({message:"Express server running"});
+});
+
+app.listen(4000,()=>{
+    console.log('Express server running on port 4000')
+})
