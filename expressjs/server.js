9920df682b2e5b2945fdce32b4d6b0f83610893c
Gobler-code <goblergoss@gmail.com>
2026-05-24 09:53:52 +0545

    Create crud api with mongodb

--- expressjs/server.js
+++ expressjs/server.js
@@ -6,15 +6,70 @@ const Note = require('./models/note');
 const app = express();
 app.use(express.json())
 
-app.get('/api/notes',async(req,res)=>{
-    const notes = await Note.find();
+app.get('/api/notes', async (req, res) => {
+    try {
+        const notes = await Note.find();
+        if (notes.length === 0) {
+            res.status(404).json({ message: 'No notes found' });
+            return;
+        }
+        res.json(notes);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+});
+
+app.get('/api/notes/:id',async(req,res)=>{
+    try{ const notes = await Note.findById(req.params.id);
+    if(!notes){
+        res.status(404).json({message:'Note not found'});
+        return;
+    }
     res.json(notes);
+   } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+   
 })
 
 app.post('/api/notes', async(req,res)=>{
-    const note = new Note(req.body);
+    try{ const note = new Note(req.body);
     await note.save();
     res.status(201).json(note);
+    }catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+   
+})
+
+app.put('/api/notes/:id',async (req,res)=>{
+ try{
+       const note = await Note.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    );
+    if(!note){
+        res.status(404).json({message:"Note not found"})
+        return;
+    }
+    res.json(note);
+ }catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+})
+
+app.delete('/api/notes/:id',async (req,res)=>{
+   try{
+     const note = await Note.findByIdAndDelete(req.params.id);
+    if(!note){
+        res.status(404).json({message:"Note not found"})
+        return;
+    }
+    res.json({message:"Note deleted",note});
+   }catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
 })
 
 mongoose.connect(process.env.MONGODB_URI)
