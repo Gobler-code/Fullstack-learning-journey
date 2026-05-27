8790a20c100466fd10c64eac697e3a67857ed390
Gobler-code <goblergoss@gmail.com>
2026-05-27 10:35:33 +0545

    Add protected CRUD routes for notes management

--- expressjs/routes/noteRoutes.js
+++ expressjs/routes/noteRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const protect  = require('../middleware/authMiddleware');
 const { getNotes, getNoteById, postNotes, putNotes, deleteNote } = require('../controllers/noteController');
 
-router.get('/', getNotes);
-router.get('/:id', getNoteById);
-router.post('/', postNotes);
-router.put('/:id',putNotes);
-router.delete('/:id',deleteNote);
+router.get('/',protect, getNotes);
+router.get('/:id',protect, getNoteById);
+router.post('/',protect, postNotes);
+router.put('/:id',protect,putNotes);
+router.delete('/:id',protect,deleteNote);
 
 module.exports = router;
